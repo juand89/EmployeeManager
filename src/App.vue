e4188899d3117202e1635d42bebf97cef623b849
Juan Diego Urena <juandu89@gmail.com>
2021-04-25 15:33:59 +0900

    implement the header to main page

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
+    <TheHeader v-if="$route.name !== 'login'" />
     <router-view />
   </div>
 </template>
 
 <script>
+import TheHeader from '@/components/TheHeader.vue'
+
 export default {
   name: 'App',
+  components: { 
+    TheHeader
+  }
 }
 </script>
