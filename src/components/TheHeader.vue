594d9e10d0fa8d483a00cfa27c0555749722f4e6
Juan Diego Urena <juandu89@gmail.com>
2021-04-25 14:37:03 +0900

    create the header component

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="bg-green-400 border-b">
+    <div class="flex justify-around w-full bg-teal p-4">
+      <span class="font-semibold text-xl tracking-tight text-white">
+        Employees Review
+      </span>
+      <div class="flex-grow flex-row-reverse  flex">
+          <button
+            class="text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-green-500 hover:bg-white "
+          >
+            Log Out
+          </button>
+      </div>
+    </div>
+  </header>
+</template>
