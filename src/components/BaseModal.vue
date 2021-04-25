3852177ca9251fee7b0cc64a0a8b2fae2bcaa69a
Juan Diego Urena <juandu89@gmail.com>
2021-04-25 17:14:39 +0900

    create base modal component

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div
+      class="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex"
+    >
+      <slot></slot>
+    </div>
+    <div class="opacity-25 fixed inset-0 z-40 bg-black" />
+  </div>
+</template>
