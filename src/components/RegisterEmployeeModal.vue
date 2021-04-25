cff262fd5bdf87b5628710875398b5b5839deac7
Juan Diego Urena <juandu89@gmail.com>
2021-04-25 20:17:41 +0900

    add close button to dismiss the modal

--- src/components/RegisterEmployeeModal.vue
+++ src/components/RegisterEmployeeModal.vue
@@ -1,70 +1,91 @@
 <template>
-  <BaseModal>
-    <form
-      class="flex flex-col justify-center space-y-5 mt-5 bg-white w-96 shadow-xl rounded p-5"
-    >
-      <div>
-        <TheInput
-          label="First Name"
-          v-model="employee.firstName"
-          type="text"
-          name="first name"
-          id="fist name"
-          placeholder="Enter first name"
-        />
+  <BaseModal @close="$emit('closeModal')">
+    <div>
+      <div
+        class="flex flex-row-reverse justify-between p-5 py-3 bg-white border-b border-gray-200 "
+      >
+        <svg
+          @click="$emit('closeModal')"
+          class="w-6 h-6 cursor-pointer"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M6 18L18 6M6 6l12 12"
+          ></path>
+        </svg>
       </div>
-      <div>
-        <TheInput
-          label="Last Name"
-          v-model="employee.lastName"
-          type="text"
-          name="last name"
-          id="position"
-          placeholder="Enter last name"
-        />
-      </div>
-      <div>
-        <TheInput
-          label="Positon"
-          v-model="employee.position"
-          type="text"
-          name="position"
-          id="position"
-          placeholder="Enter position of the employee"
-        />
-      </div>
-      <div>
-        <TheInput
-          label="Email"
-          v-model="employee.email"
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Enter email"
-        />
-      </div>
-      <div class="mt-8">
+      <form
+        class="flex flex-col justify-center space-y-5  bg-white w-96 shadow-xl p-5"
+      >
         <div>
           <TheInput
-            label="Password"
-            v-model="employee.password"
-            type="password"
-            name="password"
-            placeholder="Enter password"
+            label="First Name"
+            v-model="employee.firstName"
+            type="text"
+            name="first name"
+            id="fist name"
+            placeholder="Enter first name"
           />
         </div>
-      </div>
-      <div class="mt-10">
-        <button
-          class="bg-blue-500 text-gray-100 p-4 w-full rounded-full tracking-wide
+        <div>
+          <TheInput
+            label="Last Name"
+            v-model="employee.lastName"
+            type="text"
+            name="last name"
+            id="position"
+            placeholder="Enter last name"
+          />
+        </div>
+        <div>
+          <TheInput
+            label="Positon"
+            v-model="employee.position"
+            type="text"
+            name="position"
+            id="position"
+            placeholder="Enter position of the employee"
+          />
+        </div>
+        <div>
+          <TheInput
+            label="Email"
+            v-model="employee.email"
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Enter email"
+          />
+        </div>
+        <div class="mt-8">
+          <div>
+            <TheInput
+              label="Password"
+              v-model="employee.password"
+              type="password"
+              name="password"
+              placeholder="Enter password"
+            />
+          </div>
+        </div>
+        <div class="mt-10">
+          <button
+            class="bg-blue-500 text-gray-100 p-4 w-full rounded-full tracking-wide
                     font-semibold font-display focus:outline-none focus:shadow-outline hover:bg-blue-600
                     shadow-lg"
-          @click="registerEmployee"
-        >
-          Register Employee
-        </button>
-      </div>
-    </form>
+            @click="registerEmployee"
+          >
+            Register Employee
+          </button>
+        </div>
+      </form>
+    </div>
   </BaseModal>
 </template>
 <script>
@@ -76,7 +97,7 @@ export default {
     TheInput,
   },
   data() {
-    return { 
+    return {
       employee: {
         firstName: '',
         lastName: '',
