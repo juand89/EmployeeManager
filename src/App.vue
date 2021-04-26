e84219a0c64d2ed9d822b9f3e041db2b8beb04b7
Juan Diego Urena <juandu89@gmail.com>
2021-04-26 19:52:12 +0900

    show header only when the user is login

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <template>
   <div id="app">
-    <TheHeader v-if="$route.name !== 'login'" />
+    <TheHeader v-if="showHeader" />
     <router-view />
   </div>
 </template>
 
 <script>
+import { firebase } from '../plugins/firebase.js'
 import TheHeader from '@/components/TheHeader.vue'
-
 export default {
   name: 'App',
   components: { 
     TheHeader
-  }
+  },
+  data() {
+    return {
+      showHeader: false,
+    }
+  },
+  async mounted() {
+     if (await firebase.getCurrentUser()) {
+       this.showHeader = true;
+     }
+  },
+  watch: {
+    '$route.path' (newVal) {
+      this.showHeader = newVal !== '/login'
+    }
+  },
 }
 </script>
