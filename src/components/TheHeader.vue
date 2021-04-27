5bda7fc6a0a587129abd827148483751c52a7fa2
Juan Diego Urena <juandu89@gmail.com>
2021-04-27 19:16:28 +0900

    redirect to home when clicking the title of the header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,9 +1,9 @@
 <template>
-  <header class="bg-green-400 border-b">
+  <header class="bg-green-400 border-b ">
     <div class="flex justify-around w-full bg-teal p-4">
-      <span class="font-semibold text-xl tracking-tight text-white">
+      <router-link to="/" class="cursor-pointer font-semibold text-xl tracking-tight text-white">
         Employees Review
-      </span>
+      </router-link>
       <div class="flex-grow flex-row-reverse  flex">
           <button
             class="text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-green-500 hover:bg-white "
@@ -22,9 +22,9 @@ export default {
   methods: {
     logout() {
       auth.signOut().then(() => {
-        this.$router.push('login')
+        this.$router.go('login')
       }).catch((error) => {
-        console.log(error);
+        console.error(error);
         alert(error);
       });
     }
